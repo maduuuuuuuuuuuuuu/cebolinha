a84f57b1a073c8b5e9cb271482ca34de6ceef5b0
madu <maduamaral2006@gmail.com>
2023-06-29 12:54:09 -0300

    exercicio resolvido

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿string frase;
+string flase;
+Console.WriteLine("digite uma flase pala mim");
+frase = Console.ReadLine()!;
+flase = frase
+.Replace("R", "L")
+.Replace("r", "l");
+Console.WriteLine("");
+Console.WriteLine(flase);
+
